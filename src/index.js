252afb36b0ace4ec87a8b7e470e30360a84e3400
Galina-Goncharova <galinagoncharova1112@gmail.com>
2026-04-13 12:29:32 +0200

    init: expense tracker base structure

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
 import { ref } from "./js/refs.js";
 import { throttle } from "lodash";
 import { addExpense } from "./js/addExpense.js";
-import { createNewExpense } from "./js/addExpense.js";
 
 ref.formEl.addEventListener("submit", addExpense);
-// ref.formEl.addEventListener("input", throttle(createNewExpense));
